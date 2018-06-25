fcb8708563155663ab57343d3c5eeedb6f5eddea
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-06-25 22:10:26 +0530

    Update DataCleansing.py

--- DataCleansing.py
+++ DataCleansing.py
@@ -3,22 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file = pd.ExcelFile('C:/Users/Gaurav/Desktop/attachment_attachment_Numpy_Pandas_Assignment_updated/Numpy_Pandas_Assignment/obes-phys-acti-diet.xls')
-print (file.sheet_names)
+print (file.sheet_names)        # Fetching number of Sheets present in Excel file
 
-## Table 7.1 Finished Admission Episodes with a primary diagnosis of obesity, by gender, 2000/01 to 2011/12			
-x = file.parse(u'7.1', skiprows=5, skip_footer=14, names=['Year','Total','Males','Females','Nan', 'None'])
+## Table 7.1 Finished Admission Episodes with a primary diagnosis of obesity, by gender, 2000/01 to 2011/12		
+x = file.parse(u'7.1', skiprows=5, skip_footer=14, names=['Year','Total','Males','Females','Nan', 'None'])   # Parsing sheet 7.1
 #print (x)
-drop_7_1 = x.dropna(axis=1).set_index('Year')
+drop_7_1 = x.dropna(axis=1).set_index('Year')        # Drop NaN values
 print (drop_7_1)
-print (drop_7_1.describe())
+print (drop_7_1.describe())                          # Fetching mean, std, etc
 
 ## Table 7.2 Finished Admission Episodes with a primary diagnosis of obesity, by age group, 2000/01 to 2011/12									
 y =  file.parse(u'7.2', skiprows=4, skipfooter=14)
 #print (y)
-index_rename = y.rename(columns={'Unnamed: 0': 'Year'})
+index_rename = y.rename(columns={'Unnamed: 0': 'Year'})     # Switching Index from by-default to Year
 #print (index_rename)
-cleaned_7_2 = index_rename.drop(index_rename.index[0])
-drop_na = cleaned_7_2.dropna(axis=1).set_index('Year')
+cleaned_7_2 = index_rename.drop(index_rename.index[0])      # Drop Row using indice
+drop_na = cleaned_7_2.dropna(axis=1).set_index('Year')      # Drop NaN values Column Wise
 print (drop_na)
 print (drop_na.describe())
 
@@ -59,3 +59,5 @@ cleaned_7_7 = des.drop(des.index[7]).set_index('Year')
 print (cleaned_7_7)
 #fillna_7_7 = cleaned_7_7.fillna({'Total':0, 'Male':0, 'Female':0})
 print (cleaned_7_7.describe())
+
+# CODED BY - GAURAV_PADAWE
